a5fe2062fd8874219e19ba71da48bc0dd475fb0d
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-11 08:35:43 +0000

    firstcommit

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,22 @@
+class LogsController < ApplicationController
+    
+    def index
+        
+    end
+    
+    def show
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def destroy
+        
+    end
+end
