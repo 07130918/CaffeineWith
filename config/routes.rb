70181cc81f78e48145135803d068d78ea518f96a
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-16 23:51:00 +0000

    caffesのルートを作りlogsをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "logs#index"
-  resources :logs, only: [:index, :show, :new, :create, :destroy] do 
-    collection do
-      get :all
-    end
+  resources :caffes, only: [:index] do
+    resources :logs, only: [:show, :new, :create, :destroy] 
+      collection do
+        get :all
+      end
   end  
 end
