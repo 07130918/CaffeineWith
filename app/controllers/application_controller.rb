c8f3d496dc87c672dc34cd459c78fe5f0f4e41dd
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-20 01:22:32 +0000

    signup時nicknameとimageのパラメータ許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     # CSRF対策(クロスサイトリクエストフォージェリ)アクセスしたユーザーに対して意図しない操作を行わせないため
     
-    
+    before_action :configure_permitted_parameters, if: :devise_controller?
     
     def after_sign_out_path_for(resource)
         "/users/sign_in"
     end
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
+    end
+
 end
