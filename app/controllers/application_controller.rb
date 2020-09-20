4e379e7bd6020cd742e5123b9f68a19f92984324
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-20 01:18:48 +0000

    csrf対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
     
+    protect_from_forgery with: :exception
+    # CSRF対策(クロスサイトリクエストフォージェリ)アクセスしたユーザーに対して意図しない操作を行わせないため
+    
+    
+    
     def after_sign_out_path_for(resource)
         "/users/sign_in"
     end
