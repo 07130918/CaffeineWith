4db96d4e121d21241bf3dff79bd9b120a0c50b96
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-21 06:15:52 +0000

    今までに飲んだコーヒーをカウント

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
     
     def show
-        @logs = Log.where(user_id: current_user.id).page(params[:page]).per(7).order("created_at DESC")
+        @logs = Log.where(user_id: current_user.id)
+        @cups = Log.where(user_id: current_user.id).count
     end
     
     def edit
