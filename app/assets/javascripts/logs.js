5b369335fdf2f0789d4b39a359e3982bbe5e2da1
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-21 07:04:12 +0000

    card作成

--- app/assets/javascripts/logs.js
+++ app/assets/javascripts/logs.js
@@ -10,9 +10,12 @@ $(document).on('turbolinks:load', function() {
        
         if (menu) {
             $("#sidebarMenu").css("transform","translateX(0)");
+            $(".slide").css("margin-left","0");
         } else {
             $("#sidebarMenu").css("transform","translateX(100%)");
             $("#sidebarMenu").css("transition","transform 250ms ease-in-out");
+            $(".slide").css("margin-left","auto");
+            $(".slide").css("transition","transform 250ms ease-in-out");
         }
     });
     
