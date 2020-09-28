a55ea69e32c12e998635e46314195317fe7561ad
EC2 Default User <ec2-user@ip-172-31-1-72.ap-northeast-1.compute.internal>
2020-09-28 01:25:40 +0000

    pagination css 完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
     
     def show
-        @logs = Log.where(user_id: current_user.id).order("created_at DESC")
-        @cups = Log.where(user_id: current_user.id).count
         today = Log.created_today.where(user_id: current_user.id)
         @today = today.count
         c_id = today.pluck(:caffe_id)
         caffeine = c_id.map{|id| Caffe.find(id).caffeine_mg}
         @today_caffeine = caffeine.sum
+        @logs = Log.where(user_id: current_user.id).order("created_at DESC")
+        @cups = Log.where(user_id: current_user.id).count
     end
     
     def edit
